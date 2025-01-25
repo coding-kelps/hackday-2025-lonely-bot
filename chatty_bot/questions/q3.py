fbcb1367c66354a70f8aff5219e78a477ae6c46c
Guilhem Sante <guilhem.snt@gmail.com>
2025-01-25 19:08:44 +0100

    fix: typo in fahrenheit spelling

--- chatty_bot/questions/q3.py
+++ chatty_bot/questions/q3.py
@@ -1,12 +1,12 @@
 def q3(_chatty_bot, lonely_bot_line: str) -> str:
     """
     Return the expected response to the 3rd question of the lonely bot.
-    Give the fusion temperature of brass either in Celsius, Farenheit, Kelvin.
+    Give the fusion temperature of brass either in Celsius, Fahrenheit, Kelvin.
 
     # Context
 
     For the 3rd question the bot asks us the fusion temperature of brass,
-    it can ask us either in Celsius, Farenheit, Kelvin.
+    it can ask us either in Celsius, Fahrenheit, Kelvin.
 
     ```
     what's the fusion temperature of brass? Give it to me in Kelvin and strip off decimals
@@ -16,9 +16,9 @@ def q3(_chatty_bot, lonely_bot_line: str) -> str:
     # Source: https://fr.wikipedia.org/wiki/Laiton
     if "Celsius" in lonely_bot_line:
         return "932"
-    elif "Farenheit" in lonely_bot_line:
+    elif "Fahrenheit" in lonely_bot_line:
         return "1709"
     elif "Kelvin" in lonely_bot_line:
         return "1205"
     else:
-        raise Exception("The lonely bot question didn't contained the expected keywords (Celsius, Farenheit, or Kelvin), sending empty response")
+        raise Exception("The lonely bot question didn't contained the expected keywords (Celsius, Fahrenheit, or Kelvin), sending empty response")
