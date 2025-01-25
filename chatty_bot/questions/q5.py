07149de8c21aca595de7c158d43d57c267c2cc71
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2025-01-25 22:45:54 +0100

    feat: update for new version of the lonely bot

--- chatty_bot/questions/q5.py
+++ chatty_bot/questions/q5.py
@@ -1,7 +1,7 @@
 import logging
 import base64
 
-ENCODED_PHRASE = "I know many encoding bases, but which one i'm using...?".encode("utf-8")
+ENCODED_PHRASE = "I know many encoding bases, but which one i'm using...? (write your answer in lowercase like 'base64')".encode("utf-8")
 
 def q5(chatty_bot, lonely_bot_line: str) -> str:
     """
@@ -16,15 +16,26 @@ def q5(chatty_bot, lonely_bot_line: str) -> str:
 
     ```
     Can't have you on this point, you know your basics! We'll see if you can keep up!
-    Ng!)(Z+9SVVQzUKWo~0{WNB_^AYx&2WpgYbVs&&NcW7y2XdrKHWguxMZ6I}XX>MmOE-pU
+    SSBrbm93IG1hbnkgZW5jb2RpbmcgYmFzZXMsIGJ1dCB3aGljaCBvbmUgaSdtIHVzaW5nLi4uPyAod3JpdGUgeW91ciBhbnN3ZXIgaW4gbG93ZXJjYXNlIGxpa2UgJ2Jhc2U2NCcp
     ```
     """
     hash = chatty_bot.listen_to_lonely_bot()
     logging.debug(f"lonely bot send: \"{hash}\"")
     
-    if base64.b64decode(hash) == ENCODED_PHRASE:
-        return "base64"
-    elif base64.b85decode(hash) == ENCODED_PHRASE:
-        return "base85"
-    else:
-        raise Exception("The lonely bot hash didn't correspond to any encoding base, sending empty response")
+    try:
+        if base64.b32decode(hash) == ENCODED_PHRASE:
+            return "base32"
+    except:
+        0
+    try:
+        if base64.b64decode(hash) == ENCODED_PHRASE:
+            return "base64"
+    except:
+        0
+    try:
+        if base64.b85decode(hash) == ENCODED_PHRASE:
+            return "base85"
+    except:
+        0
+    
+    raise Exception("The lonely bot hash didn't correspond to any encoding base, sending empty response")
